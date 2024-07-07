4bed2c11bf0f17e222c6d2cc16ccca2c5cd0ec27
koora sai sujith <121951118+saisujith3845@users.noreply.github.com>
2024-07-07 09:37:18 -0700

    Delete backend directory

--- backend/models/Notification.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const notificationSchema = new mongoose.Schema({
-    user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    message: { type: String, required: true },
-    date: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('Notification', notificationSchema);
